d704657e0514daa04fc601658773effb351800dc
agamboa2019 <agamboa@accuhealth.com.ar>
2020-01-23 14:40:43 -0300

    Form redux

--- src/containers/CustomerContainer.js
+++ src/containers/CustomerContainer.js
@@ -2,14 +2,31 @@ import React, { Component } from 'react';
 import { connect } from "react-redux";
 import AppFrame from '../components/AppFrame';
 import PropTypes from 'prop-types';
+import { getCustomerById } from '../selector/customers';
+import { Route } from 'react-router-dom';
+import CustomerEdit from "./../components/CustomerEdit";
+import CustomerData from "./../components/CustomerData";
+import { withRouter } from 'react-router-dom';
 
 class CustomerContainer extends Component {
+    renderBody = () => (
+        <Route 
+            path="/customers/:dni/edit"
+            children={
+                ({match}) => {
+                    const CustomerControl = match ? CustomerEdit : CustomerData;
+                    return <CustomerControl {...this.props.customer} />
+                }
+            }
+        ></Route>
+    )
+    //<p>Datos del Cliente "{this.props.customer.name}"</p>
     render() {
         return (
             <div>
                 <AppFrame
                     header={`Cliente ${this.props.dni}`}
-        body={<p>Datos del Cliente "{this.props.customer.name}"</p>}
+                    body={this.renderBody()}
                 ></AppFrame>
             </div>
         );
@@ -22,7 +39,7 @@ CustomerContainer.propTypes = {
 };
 
 const mapStateToProps = (state, props) => ({
-    customer : state.customers.find( c => c.dni === props.dni)
+    customer : getCustomerById(state, props)
 })
 
-export default connect(mapStateToProps, null)(CustomerContainer);
+export default withRouter(connect(mapStateToProps, null)(CustomerContainer));
--- src/selector/customers.js
+++ src/selector/customers.js
@@ -1,2 +1,8 @@
+import { createSelector } from "reselect";
 
 export const getCustomers = state => state.customers;
+
+export const getCustomerById = createSelector(
+    (state, props) => state.customers.find( c => c.dni === props.dni),
+    customer => customer
+)
